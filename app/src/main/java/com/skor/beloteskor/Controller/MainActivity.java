f52571c87ca739daecca4a9801c71b7e78dc4a17
Thierry Couilleault <thierryc973@gmail.com>
2018-03-29 19:08:20 +0200

    Gestion des items de la navigationBar et du menu de la Toolbar mais vide

--- app/src/main/java/com/skor/beloteskor/Controller/MainActivity.java
+++ app/src/main/java/com/skor/beloteskor/Controller/MainActivity.java
@@ -1,15 +1,103 @@
 package com.skor.beloteskor.Controller;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.skor.beloteskor.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private android.support.v7.widget.Toolbar toolbar;
+    private BottomNavigationView navigation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //gestion de la Toolbar
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("BeloteSkor");
+
+        //gestion de la bottom navigation view
+        navigation = findViewById(R.id.navigation);
+        navigation.setSelectedItemId(R.id.scores);
+        //Navigation dans la Bootom view
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+
+                    case R.id.players:
+
+                        Toast.makeText(MainActivity.this, "Players", Toast.LENGTH_SHORT).show();
+
+                        return true;
+
+                    case R.id.scores:
+
+                        Toast.makeText(MainActivity.this, "Scores", Toast.LENGTH_SHORT).show();
+
+                        return true;
+
+                    case R.id.statistics:
+
+                        Toast.makeText(MainActivity.this, "Statistics", Toast.LENGTH_SHORT).show();
+
+                        return true;
+
+                    default:
+                        Toast.makeText(MainActivity.this, "default", Toast.LENGTH_SHORT).show();
+                        return false;
+
+                }
+
+            }
+        });
+
+
+
+
+    }
+
+    //Méthode qui inflate le menu d'option
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_toolbar, menu);
+
+        return true;
+    }
+
+
+//Méthode qui gère la sélection de le menu de la Toolbar
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.preferences:
+
+                return true;
+
+            case R.id.app_bar_search:
+
+                return true;
+
+            case R.id.logout:
+
+                return true;
+
+            default:
+            return super.onOptionsItemSelected(item);
+        }
     }
 }
