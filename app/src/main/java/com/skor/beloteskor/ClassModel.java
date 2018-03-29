e63c8be56f2b63cba7a2aec1b610d93da1b7876f
Thierry Couilleault <thierryc973@gmail.com>
2018-03-29 17:06:13 +0200

    initial commit avec Bottom navigation

---
+++ app/src/main/java/com/skor/beloteskor/ClassModel.java
@@ -0,0 +1,19 @@
+package com.skor.beloteskor;
+
+/**
+ * Created by thierrycouilleault on 29/03/2018.
+ */
+
+public class ClassModel {
+
+    //Déclaration
+    //Variables d'instances
+
+    //Constructeurs
+
+
+    //getters and setters
+
+
+
+}
